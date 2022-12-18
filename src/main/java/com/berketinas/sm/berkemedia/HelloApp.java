4c5b4e241a8801524db710209aa0c839a5669062
Berke Tınas <berke_750@hotmail.com>
2022-12-18 11:51:28 +0300

    final push

--- src/main/java/com/berketinas/sm/berkemedia/HelloApp.java
+++ src/main/java/com/berketinas/sm/berkemedia/HelloApp.java
@@ -1,8 +1,8 @@
-package com.berketinas.sm.berkemedia;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApp extends Application {
-}
+//package com.berketinas.sm.berkemedia;
+//
+//import jakarta.ws.rs.ApplicationPath;
+//import jakarta.ws.rs.core.Application;
+//
+//@ApplicationPath("/api")
+//public class HelloApp extends Application {
+//}
